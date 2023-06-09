29997c4a3d55945fa84bb39e1ab2236c12935ec7
Violetta Prikhno <Violetta_Prikhno@epam.com>
2023-06-09 14:25:02 +0300

    My first commit!

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,22 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> hashSet = new HashSet<>();
+
+        for (int num : sourceList) {
+            if (num % 2 == 0) {
+                hashSet.add(num);
+                int divisor = num;
+                while (divisor % 2 == 0 && divisor % 2 != 1) {
+                    divisor /= 2;
+                    hashSet.add(divisor);
+                }
+            } else {
+                hashSet.add(num);
+                hashSet.add(2 * num);
+            }
+        }
+
+        return hashSet;
     }
 }
